2fc9771dc28928963c42a8640286218e6f09b4e8
Checksum <me@checksum.space>
2023-06-23 01:48:30 -0500

    fix: policy

--- src/user.rs
+++ src/user.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
-use std::ops::Add;
+
+
 
 use super::err::Result;
 use serde_derive::Deserialize;
@@ -12,203 +12,112 @@ use crate::err::JellyfinError;
 use crate::JellyfinClient;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "PascalCase")]
 pub struct User {
-    #[serde(rename = "Name")]
     pub name: String,
-    #[serde(rename = "ServerId")]
     pub server_id: String,
-    #[serde(rename = "ServerName")]
     pub server_name: Option<String>,
-    #[serde(rename = "Id")]
     pub id: String,
-    #[serde(rename = "PrimaryImageTag")]
     pub primary_image_tag: Option<String>,
-    #[serde(rename = "HasPassword")]
     pub has_password: bool,
-    #[serde(rename = "HasConfiguredPassword")]
     pub has_configured_password: bool,
-    #[serde(rename = "HasConfiguredEasyPassword")]
     pub has_configured_easy_password: bool,
-    #[serde(rename = "EnableAutoLogin")]
     pub enable_auto_login: bool,
-    #[serde(rename = "LastLoginDate")]
     pub last_login_date: Option<String>,
-    #[serde(rename = "LastActivityDate")]
     pub last_activity_date: Option<String>,
-    #[serde(rename = "Configuration")]
     pub configuration: UserConfiguration,
-    #[serde(rename = "Policy")]
     pub policy: UserPolicy,
-    #[serde(rename = "PrimaryImageAspectRatio")]
     pub primary_image_aspect_ratio: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "PascalCase")]
 pub struct UserConfiguration {
-    #[serde(rename = "AudioLanguagePreference")]
     pub audio_language_preference: Option<String>,
-    #[serde(rename = "PlayDefaultAudioTrack")]
     pub play_default_audio_track: bool,
-    #[serde(rename = "SubtitleLanguagePreference")]
     pub subtitle_language_preference: String,
-    #[serde(rename = "DisplayMissingEpisodes")]
     pub display_missing_episodes: bool,
-    #[serde(rename = "GroupedFolders")]
     pub grouped_folders: Vec<String>,
-    #[serde(rename = "SubtitleMode")]
     pub subtitle_mode: String,
-    #[serde(rename = "DisplayCollectionsView")]
     pub display_collections_view: bool,
-    #[serde(rename = "EnableLocalPassword")]
     pub enable_local_password: bool,
-    #[serde(rename = "OrderedViews")]
     pub ordered_views: Vec<String>,
-    #[serde(rename = "LatestItemsExcludes")]
     pub latest_items_excludes: Vec<String>,
-    #[serde(rename = "MyMediaExcludes")]
     pub my_media_excludes: Vec<String>,
-    #[serde(rename = "HidePlayedInLatest")]
     pub hide_played_in_latest: bool,
-    #[serde(rename = "RememberAudioSelections")]
     pub remember_audio_selections: bool,
-    #[serde(rename = "RememberSubtitleSelections")]
     pub remember_subtitle_selections: bool,
-    #[serde(rename = "EnableNextEpisodeAutoPlay")]
     pub enable_next_episode_auto_play: bool,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "PascalCase")]
 pub struct UserPolicy {
-    #[serde(rename = "IsAdministrator")]
     pub is_administrator: bool,
-    #[serde(rename = "IsHidden")]
     pub is_hidden: bool,
-    #[serde(rename = "IsDisabled")]
     pub is_disabled: bool,
-    #[serde(rename = "MaxParentalRating")]
     pub max_parental_rating: Option<i64>,
-    #[serde(rename = "BlockedTags")]
     pub blocked_tags: Vec<String>,
-    #[serde(rename = "EnableUserPreferenceAccess")]
     pub enable_user_preference_access: bool,
-    #[serde(rename = "AccessSchedules")]
     pub access_schedules: Vec<UserAccessSchedule>,
-    #[serde(rename = "BlockUnratedItems")]
     pub block_unrated_items: Vec<String>,
-    #[serde(rename = "EnableRemoteControlOfOtherUsers")]
     pub enable_remote_control_of_other_users: bool,
-    #[serde(rename = "EnableSharedDeviceControl")]
     pub enable_shared_device_control: bool,
-    #[serde(rename = "EnableRemoteAccess")]
     pub enable_remote_access: bool,
-    #[serde(rename = "EnableLiveTvManagement")]
     pub enable_live_tv_management: bool,
-    #[serde(rename = "EnableLiveTvAccess")]
     pub enable_live_tv_access: bool,
-    #[serde(rename = "EnableMediaPlayback")]
     pub enable_media_playback: bool,
-    #[serde(rename = "EnableAudioPlaybackTranscoding")]
     pub enable_audio_playback_transcoding: bool,
-    #[serde(rename = "EnableVideoPlaybackTranscoding")]
     pub enable_video_playback_transcoding: bool,
-    #[serde(rename = "EnablePlaybackRemuxing")]
     pub enable_playback_remuxing: bool,
-    #[serde(rename = "ForceRemoteSourceTranscoding")]
     pub force_remote_source_transcoding: bool,
-    #[serde(rename = "EnableContentDeletion")]
     pub enable_content_deletion: bool,
-    #[serde(rename = "EnableContentDeletionFromFolders")]
     pub enable_content_deletion_from_folders: Vec<String>,
-    #[serde(rename = "EnableContentDownloading")]
     pub enable_content_downloading: bool,
-    #[serde(rename = "EnableSyncTranscoding")]
     pub enable_sync_transcoding: bool,
-    #[serde(rename = "EnableMediaConversion")]
     pub enable_media_conversion: bool,
-    #[serde(rename = "EnabledDevices")]
     pub enabled_devices: Vec<String>,
-    #[serde(rename = "EnableAllDevices")]
     pub enable_all_devices: bool,
-    #[serde(rename = "EnabledChannels")]
     pub enabled_channels: Vec<String>,
-    #[serde(rename = "EnableAllChannels")]
     pub enable_all_channels: bool,
-    #[serde(rename = "EnabledFolders")]
     pub enabled_folders: Vec<String>,
-    #[serde(rename = "EnableAllFolders")]
     pub enable_all_folders: bool,
-    #[serde(rename = "InvalidLoginAttemptCount")]
     pub invalid_login_attempt_count: i64,
-    #[serde(rename = "LoginAttemptsBeforeLockout")]
     pub login_attempts_before_lockout: i64,
-    #[serde(rename = "MaxActiveSessions")]
     pub max_active_sessions: i64,
-    #[serde(rename = "EnablePublicSharing")]
     pub enable_public_sharing: bool,
-    #[serde(rename = "BlockedMediaFolders")]
     pub blocked_media_folders: Vec<String>,
-    #[serde(rename = "BlockedChannels")]
     pub blocked_channels: Vec<String>,
-    #[serde(rename = "RemoteClientBitrateLimit")]
     pub remote_client_bitrate_limit: i64,
-    #[serde(rename = "AuthenticationProviderId")]
     pub authentication_provider_id: String,
-    #[serde(rename = "PasswordResetProviderId")]
     pub password_reset_provider_id: String,
-    #[serde(rename = "SyncPlayAccess")]
     pub sync_play_access: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "PascalCase")]
 pub struct UserAccessSchedule {
-    #[serde(rename = "UserId")]
     pub user_id: String,
-    #[serde(rename = "DayOfWeek")]
     pub day_of_week: String,
-    #[serde(rename = "StartHour")]
     pub start_hour: i64,
-    #[serde(rename = "EndHour")]
     pub end_hour: i64,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "PascalCase")]
 pub struct UserAuth {
-    #[serde(rename = "User")]
     pub user: User,
-    #[serde(rename = "SessionInfo")]
     pub session_info: SessionInfo,
-    #[serde(rename = "AccessToken")]
     pub access_token: String,
-    #[serde(rename = "ServerId")]
     pub server_id: String,
 }
 
 impl UserAuth {
     pub fn to_emby_header(&self) -> String {
-        let device_name = whoami::devicename().replace(" ", "_");
+        let device_name = whoami::devicename().replace(' ', "_");
         format!("Emby UserId=\"{}\", Client=\"jellyfin-rs\", Device=\"{}\", DeviceId=\"{:x}\", Version=1, Token=\"{}\"", self.user.id, device_name, md5::compute(device_name.clone()), self.access_token)
     }
 }
-
-// static GET_USERS_EP: &str = "/Users";
-// static GET_USER_BY_ID_EP: &str = "/Users/{id}";
-// static DELETE_USER_EP: &str = "/Users/{id}";
-// static UPDATE_USER_EP: &str = "/Users/{id}";
-// static AUTH_USER_STD_EP: &str = "/Users/{id}/Authenticate";
-// static UPDATE_USER_CONF_EP: &str = "/Users/{id}/Configutation";
-// static UPDATE_USER_PASSWORD_EP: &str = "/Users/{id}/Password";
-// static UPDATE_USER_POLICY_EP: &str = "/Users/{id}/Policy";
-// static AUTH_USER_NAME_EP: &str = "/Users/AuthenticateByName";
-// static USER_FORGOT_PASSWORD_EP: &str = "/Users/ForgotPassword";
-// static USER_REDEEM_FORGOT_PASSWORD_PIN_EP: &str = "/Users/ForgotPassword/Pin";
-// static GET_USER_BY_AUTH_EP: &str = "/Users/Me";
-// static CREATE_USER_EP: &str = "/Users/New";
-// static GET_PUBLIC_USER_LIST_EP: &str = "/Users/Public";
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct GetUsersQuery {
@@ -272,7 +181,7 @@ impl JellyfinClient {
     }
 
     pub async fn delete_user<T: Into<String>>(&self, id: T) -> Result<()> {
-        let req = self
+        let _req = self
             .client
             .delete(self.url.join("/Users")?.join(id.into().as_str())?)
             .header(
@@ -289,7 +198,7 @@ impl JellyfinClient {
     }
 
     pub async fn update_user<T: Into<String>>(&self, id: T, new_info: User) -> Result<()> {
-        let req = self
+        let _req = self
             .client
             .post(self.url.join("/Users")?.join(id.into().as_str())?)
             .json(&new_info)
@@ -313,7 +222,6 @@ impl JellyfinClient {
     ) -> Result<()> {
         let mut hasher = sha1::Sha1::new();
         hasher.update(password.clone().into());
-
         let device_name = whoami::devicename().replace(" ", "_");
 
         let req = self
@@ -334,7 +242,7 @@ impl JellyfinClient {
         id: T,
         new_conf: UserConfiguration,
     ) -> Result<()> {
-        let req = self
+        let _req = self
             .client
             .post(
                 self.url
@@ -361,7 +269,7 @@ impl JellyfinClient {
         id: T,
         new_password: T,
     ) -> Result<()> {
-        let req = self
+        let _req = self
             .client
             .post(
                 self.url
@@ -388,7 +296,7 @@ impl JellyfinClient {
         id: T,
         new_policy: UserPolicy,
     ) -> Result<()> {
-        let req = self
+        let _req = self
             .client
             .post(
                 self.url
@@ -415,7 +323,7 @@ impl JellyfinClient {
         username: T,
         password: T,
     ) -> Result<()> {
-        let device_name = whoami::devicename().replace(" ", "_");
+        let device_name = whoami::devicename().replace(' ', "_");
 
         let req = self.client.post(self.url.join("/Users/AuthenticateByName")?)
             .json(&AuthUserNameQuery {
@@ -431,9 +339,9 @@ impl JellyfinClient {
     }
 
     pub async fn user_forgot_password<T: Into<String>>(&self, username: T) -> Result<()> {
-        let device_name = whoami::devicename().replace(" ", "_");
+        let device_name = whoami::devicename().replace(' ', "_");
 
-        let req = self.client.post(self.url.join("/Users/SendPasswordResetEmail")?)
+        let _req = self.client.post(self.url.join("/Users/SendPasswordResetEmail")?)
             .json(&json!({
                 "EnteredUsername": username.into()
             }))
@@ -445,9 +353,9 @@ impl JellyfinClient {
     }
 
     pub async fn user_redeem_forgot_password_pin<T: Into<String>>(&self, pin: T) -> Result<()> {
-        let device_name = whoami::devicename().replace(" ", "_");
+        let device_name = whoami::devicename().replace(' ', "_");
 
-        let req = self.client.post(self.url.join("/Users/RedeemPasswordResetToken")?)
+        let _req = self.client.post(self.url.join("/Users/RedeemPasswordResetToken")?)
             .json(&json!({
                 "Pin": pin.into()
             }))
@@ -497,7 +405,7 @@ impl JellyfinClient {
     }
 
     pub async fn get_public_user_list(&self) -> Result<Vec<User>> {
-        let device_name = whoami::devicename().replace(" ", "_");
+        let device_name = whoami::devicename().replace(' ', "_");
 
         let req = self.client.get(self.url.join("/Users/Public")?)
             .header("X-Emby-Authorization", format!("Emby UserId=\"\", Client=\"jellyfin-rs\", Device=\"{}\", DeviceId=\"{:x}\", Version=1, Token=\"\"", device_name, md5::compute(device_name.clone())))
